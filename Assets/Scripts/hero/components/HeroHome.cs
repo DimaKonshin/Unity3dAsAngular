138d4ae6ffea42cd47adee8e79222603b5d82ace
Dima Konshin <dimadancev97@gmail.com>
2020-02-23 19:34:47 +0300

    adding skeleton

---
+++ Assets/Scripts/hero/components/HeroHome.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroHome : MonoBehaviour
+{
+    [SerializeField] GameObject timer;
+    [SerializeField] float delayTime = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InjectFn();
+    }
+
+    private void InjectFn()
+    {
+        //Instantiate(timer).GetComponent<Timer>().CallFunctionAfterSeconds(delayTime, () => {
+        //    Debug.Log("Stop");
+        //});
+        timer.GetComponent<Timer>().CallFunctionAfterSeconds(delayTime, () => {
+            Debug.Log("Stop");
+            Destroy(timer);
+        });
+    }
+}
