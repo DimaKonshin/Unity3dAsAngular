ab523b9bcd6cf3c0ddbe2c29fa67ae08eed8a375
Dima Konshin <dimadancev97@gmail.com>
2020-02-24 10:53:16 +0300

    adding mover

--- Assets/Scripts/hero/components/HeroHome.cs
+++ Assets/Scripts/hero/components/HeroHome.cs
@@ -7,21 +7,30 @@ public class HeroHome : MonoBehaviour
     [SerializeField] GameObject timer;
     [SerializeField] float delayTime = 2;
 
-    void Start()
-    {
-        InjectFn();
-    }
+    [SerializeField] Transform targetTransform;
+    [SerializeField] GameObject moverGO;
 
-    private void InjectFn()
+    void Start()
     {
         timer.GetComponent<Timer>().CallFunctionAfterSeconds(delayTime, () => {
             CallFunctionAfterSeconds();
         });
+    }
+
+    void Update()
+    {
+        MoveToWards();
     }
     
     private void CallFunctionAfterSeconds()
     {
         Debug.Log("Stop " + CalculatePoints.GetPoints());
         Destroy(timer);
+    }
+
+    private void MoveToWards()
+    {
+        Mover mover = moverGO.GetComponent<Mover>();
+        mover.ParentMoveToWards(targetTransform);
     }
 }
