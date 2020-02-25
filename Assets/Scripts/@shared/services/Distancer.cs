d3bd677198872ac3dff54333927fe30ccacf8b3e
Dima Konshin <dimadancev97@gmail.com>
2020-02-25 22:41:30 +0300

    adding distancer

---
+++ Assets/Scripts/@shared/services/Distancer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class Distancer : MonoBehaviour
+{
+    [SerializeField] Transform _targetTransform;
+    [SerializeField] float _distance = 0f;
+    public Action<Transform> OnDistanceMore;
+    public Action<Transform> OnDistanceLess;
+    public Action<Transform> OnDistanceEqual;
+
+    public void Constructor(Transform targetTransform, float distance)
+    {
+        _targetTransform = targetTransform;
+        _distance = distance;
+    }
+
+    void Update()
+    {
+        if (_targetTransform == null)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(transform.parent.position, _targetTransform.position) > _distance)
+        {
+            OnDistanceMore?.Invoke(_targetTransform);
+        }
+        else if (Vector3.Distance(transform.parent.position, _targetTransform.position) < _distance)
+        {
+            OnDistanceLess?.Invoke(_targetTransform);
+        }
+        else if (Vector3.Distance(transform.parent.position, _targetTransform.position) == _distance)
+        {
+            OnDistanceEqual?.Invoke(_targetTransform);
+        }
+    }
+}
--- Assets/Scripts/hero/components/HeroHome.cs
+++ Assets/Scripts/hero/components/HeroHome.cs
@@ -4,33 +4,41 @@ using UnityEngine;
 
 public class HeroHome : MonoBehaviour
 {
+    [SerializeField] Transform targetTransform;
+
     [SerializeField] GameObject timer;
     [SerializeField] float delayTime = 2;
 
-    [SerializeField] Transform targetTransform;
     [SerializeField] GameObject moverGO;
+    Mover mover;
+
+    [SerializeField] GameObject distancerGO;
+    [SerializeField] float targetDistance = 1f;
+    Distancer distancer;
 
     void Start()
     {
+        mover = moverGO.GetComponent<Mover>();
+
         timer.GetComponent<Timer>().CallFunctionAfterSeconds(delayTime, () => {
             CallFunctionAfterSeconds();
         });
+
+        InitializeDistancer();
     }
 
-    void Update()
-    {
-        MoveToWards();
-    }
-    
     private void CallFunctionAfterSeconds()
     {
         Debug.Log("Stop " + CalculatePoints.GetPoints());
         Destroy(timer);
     }
 
-    private void MoveToWards()
+    private void InitializeDistancer()
     {
-        Mover mover = moverGO.GetComponent<Mover>();
-        mover.ParentMoveToWards(targetTransform);
+        //GameObject go = Instantiate(distancerGO); // use clone Distancer
+        //go.transform.SetParent(gameObject.transform);
+        distancer = distancerGO.GetComponent<Distancer>(); // use existing Distancer
+        distancer.Constructor(targetTransform, targetDistance);
+        distancer.OnDistanceMore += mover.ParentMoveToWards;
     }
 }
